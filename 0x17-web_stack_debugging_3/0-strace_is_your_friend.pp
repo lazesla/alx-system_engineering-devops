734046f8b0458855c909d5fc7ad44045708656a0
eslalaz <eslalazarus23@gmail.com>
2023-08-31 07:58:06 -0700

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,20 @@
-#Fix the log Errors, catch the error and fixe it
-exec {'/etc/php5/apache2/php.ini':
-  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
-  command => "sed -i 's/display_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini",
+# Attach strace to the Apache process with PID
+sudo strace -p <apache_pid> -o apache_strace.log
+curl -sI 127.0.0.1
+
+# content of 0-strace_is_your_friend.pp
+file { '/etc/apache2/sites-available/your-site.conf':
+  ensure  => file,
+  source  => 'puppet:///modules/your_module/your-site.conf', # Upload your fixed site configuration file to this path on the Puppet server
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  require => Package['apache2'],
+  notify  => Service['apache2'],
 }
 
-exec { '/var/www/html/wp-setting.php':
-  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
-  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-}
-
-exec {'/etc/init.d/apache2':
-  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-  command => '/etc/init.d/apache2 restart',
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/apache2/sites-available/your-site.conf'],
 }
