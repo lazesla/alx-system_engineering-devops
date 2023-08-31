a459b5bb838546a228e2b4719ab5dccb385b1c72
eslalaz <eslalazarus23@gmail.com>
2023-08-31 08:10:02 -0700

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,12 @@
-# Fixes wordpress config file typo
-exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
+# turn on display_errors in /etc/php5/apach2/php.ini
+exec { 'fix typo,  extra p in php extension':
+  command => 'sed  -i "s@.phpp@.php@" /var/www/html/wp-settings.php',
+  path    => ['/bin','/sbin','usr/bin','usr/sbin']
+}
+
+# to use this, first run 'puppet module install puppetlabs-stdlib --version 4.19.0'
+# file_line { 'fix typo,  extra p in php extension':
+#  path  => '/var/www/html/wp-settings.php',
+#  line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
+#  match => 'require_once\( ABSPATH . WPINC . \'/class-wp-locale.phpp\' \);'
+#}
