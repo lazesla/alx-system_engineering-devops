50b9d44961c716d7ff25fbb7e862d4c841fe42df
eslalaz <eslalazarus23@gmail.com>
2023-08-31 08:06:46 -0700

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,2 @@
-# Attach strace to the Apache process with PID
-sudo strace -p <apache_pid> -o apache_strace.log
-curl -sI 127.0.0.1
-
-# content of 0-strace_is_your_friend.pp
-file { '/etc/apache2/sites-available/your-site.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/your_module/your-site.conf', # Upload your fixed site configuration file to this path on the Puppet server
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  require => Package['apache2'],
-  notify  => Service['apache2'],
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/apache2/sites-available/your-site.conf'],
-}
+# Fixes wordpress config file typo
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
